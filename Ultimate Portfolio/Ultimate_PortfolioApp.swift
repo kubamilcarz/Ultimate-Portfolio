f3c6becd0f2b23a3dee6e02eca827e46478a878b
Kuba Milcarz <kubamilcarz@yahoo.com>
2023-10-25 09:43:33 -0400

    Initial Commit

---
+++ Ultimate Portfolio/Ultimate_PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ultimate_PortfolioApp.swift
+//  Ultimate Portfolio
+//
+//  Created by Kuba Milcarz on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Ultimate_PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
