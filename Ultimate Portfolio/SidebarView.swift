c4a3be1784b432ccf56e8d25c7e3e37167843404
Kuba Milcarz <kubamilcarz@yahoo.com>
2023-10-25 21:08:24 -0400

    Initial commit

--- Ultimate Portfolio/SidebarView.swift
+++ Ultimate Portfolio/SidebarView.swift
@@ -8,11 +8,49 @@
 import SwiftUI
 
 struct SidebarView: View {
+    @EnvironmentObject var dataController: DataController
+    let smartFilters: [Filter] = [.all, .recent]
+    
+    @FetchRequest(sortDescriptors: [SortDescriptor(\.name)]) var tags: FetchedResults<Tag>
+    
+    var tagFilters: [Filter] {
+        tags.map { tag in
+            Filter(
+                id: tag.id ?? UUID(),
+                name: tag.name ?? "No name",
+                icon: "tag",
+                tag: tag)
+        }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(selection: $dataController.selectedFilter) {
+            Section("Smart Filter") {
+                ForEach(smartFilters) { filter in
+                    NavigationLink(value: filter) {
+                        Label(filter.name, systemImage: filter.icon)
+                    }
+                }
+            }
+            
+            Section("Tags") {
+                ForEach(tagFilters) { filter in
+                    NavigationLink(value: filter) {
+                        Label(filter.name, systemImage: filter.icon)
+                    }
+                }
+            }
+        }
+        .toolbar {
+            Button("Add Samples", systemImage: "flame") {
+                dataController.deleteAll()
+                dataController.createSampleData()
+            }
+        }
     }
 }
 
 #Preview {
     SidebarView()
+        .environmentObject(DataController.preview)
 }
