36249cec73a2313105c42aeb5f9c64db609e7c2f
Kuba Milcarz <kubamilcarz@yahoo.com>
2023-10-26 22:36:59 -0400

    search functionality with NSPredicate and NSCombinedPredicate

---
+++ Ultimate Portfolio/IssueView.swift
@@ -0,0 +1,18 @@
+//
+//  IssueView.swift
+//  Ultimate Portfolio
+//
+//  Created by Kuba Milcarz on 10/26/23.
+//
+
+import SwiftUI
+
+struct IssueView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IssueView()
+}
---
+++ Ultimate Portfolio/NoIssueView.swift
@@ -0,0 +1,18 @@
+//
+//  NoIssueView.swift
+//  Ultimate Portfolio
+//
+//  Created by Kuba Milcarz on 10/26/23.
+//
+
+import SwiftUI
+
+struct NoIssueView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoIssueView()
+}
